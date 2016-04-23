6e472ce751526f6c128d202f19b0ea8747db1216
Brandon <tricksterguy@live.com>
2016-04-23 13:19:11 -0700

    First two stages of tetris attack puzzles

---
+++ source/puzzles/basic_puzzle.hpp
@@ -0,0 +1,21 @@
+#ifndef BASIC_PUZZLE_HPP
+#define BASIC_PUZZLE_HPP
+
+struct BasicPuzzle
+{
+    char magic[4];
+    char version[2];
+    char type;
+    char pad1;
+    int rows;
+    int columns;
+    int starting;
+    int moves;
+    char panels[66];
+    char pad2[6];
+};
+
+
+extern char* ta_stages[20];
+
+#endif
