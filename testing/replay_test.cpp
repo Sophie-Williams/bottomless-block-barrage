7224a488c7496417907af05eb8735c7a34c7587a
Brandon <tricksterguy87@gmail.com>
2017-07-10 00:13:11 -0700

    Remove rise.trace

--- testing/replay_test.cpp
+++ testing/replay_test.cpp
@@ -97,7 +97,6 @@ bool RunAndVerifyTrace(const std::string& trace_path)
 BOOST_AUTO_TEST_CASE(TestTraces)
 {
     BOOST_REQUIRE(RunAndVerifyTrace("traces/swap.trace"));
-    BOOST_REQUIRE(RunAndVerifyTrace("traces/rise.trace"));
     BOOST_REQUIRE(RunAndVerifyTrace("traces/swapfall.trace"));
     BOOST_REQUIRE(RunAndVerifyTrace("traces/swapfall2.trace"));
     BOOST_REQUIRE(RunAndVerifyTrace("traces/match3.trace"));
